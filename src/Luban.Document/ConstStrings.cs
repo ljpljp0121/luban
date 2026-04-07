2436270faec3b2d85c120d19860e066e98298b64
廖建鹏(深度思考中) <2540297235@qq.com>
2026-04-07 15:43:23 +0800

    Revert "[improve] 文档生成支持"
    This reverts commit 11c4a5640a10482c0e51a5ab0877f3f9cf8f1f4c.

--- src/Luban.Document/ConstStrings.cs
+++
@@ -1,9 +0,0 @@
-namespace Luban.Document;
-
-public static class ConstStrings
-{
-    public const string ArrayTypeName = "array";
-    public const string ListTypeName = "list";
-    public const string SetTypeName = "set";
-    public const string MapTypeName = "map";
-}
